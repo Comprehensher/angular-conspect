d23067b29d50b7ff3e4fb5cedc831cbaa7afa20b
Comprehensher <boltankove@gmail.com>
2023-09-30 15:31:48 +0300

    Creation angular components, binging from html to component.ts from component.ts to html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+  title = 'Dynamic title'
+  number = 41
+  ar = [1, 2, 3]
+  obj = { a: 1, b: {c: 2}}
+
+  // img = 'https://cdn.icon-icons.com/icons2/3660/PNG/512/programming_tecnology_react_logo_native_icon_228491.png'
+
+  inputValue = ''
+
+  constructor() {
+    // setTimeout(() => {
+    //   console.log('Timeout is over')
+    //   this.img = "https://www.bigroomstudios.com/wp-content/uploads/2017/08/angular-logo-big-room-studios.png"
+    // }, 5000)
+  }
+
+  onInput(event: any) {
+    this.inputValue = event.target.value
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str
+  }
+  onclick() {
+    console.log('Click')
+  }
 }
