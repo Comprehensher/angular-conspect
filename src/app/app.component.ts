f2a1722078c0e972abf65e8216d21c1c6a2b1fe1
Comprehensher <boltankove@gmail.com>
2023-09-30 15:44:44 +0300

    Examples of two way binding. From html to js; from js to htmp - rough way. See next commit with elegant way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title'
-  number = 41
-  ar = [1, 2, 3]
-  obj = { a: 1, b: {c: 2}}
+  title = 'Initial'
 
-  // img = 'https://cdn.icon-icons.com/icons2/3660/PNG/512/programming_tecnology_react_logo_native_icon_228491.png'
-
-  inputValue = ''
-
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over')
-    //   this.img = "https://www.bigroomstudios.com/wp-content/uploads/2017/08/angular-logo-big-room-studios.png"
-    // }, 5000)
-  }
-
-  onInput(event: any) {
-    this.inputValue = event.target.value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
-  onclick() {
-    console.log('Click')
-  }
+   onInput(event: any) {
+      this.title = event.target.value
+   }
 }
