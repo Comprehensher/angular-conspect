6b0dcbfd10766b31b0805459fc5a1c44a3761557
Comprehensher <boltankove@gmail.com>
2023-10-01 13:44:17 +0300

    Make css style of <h2> only for Post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export interface Post {
 export class AppComponent implements OnInit {
   posts: Post[] = [
     {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
-    // {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
+    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
   ]
 
   ngOnInit(): void {
