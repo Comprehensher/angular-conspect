511b55daea185eccd7413f0fa8912595b7b12a51
Comprehensher <boltankove@gmail.com>
2023-10-01 17:33:58 +0300

    Pass parameters-objects into directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -6,7 +6,7 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/co
 })
 export class StyleDirective {
   @Input('appStyle') color: string = 'blue'
-  @Input() fontWeight = 'normal'
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string}
   constructor(private el: ElementRef, private r: Renderer2) {
   }
   // передаем 1 -ый параметр в декортаор строку,
@@ -18,11 +18,15 @@ export class StyleDirective {
 
   @HostListener('mouseenter') onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', this.color)
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null)
     this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+    this.r.setStyle(this.el.nativeElement, 'border', null)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 }
