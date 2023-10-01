d50222cc3a9d0eeb093699c4f6deb16f652c7bca
Comprehensher <boltankove@gmail.com>
2023-10-01 17:25:35 +0300

    Pass parameters into directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,10 +1,12 @@
-import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
 
 // use [] when it need to create not tag but attribute
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'blue'
+  @Input() fontWeight = 'normal'
   constructor(private el: ElementRef, private r: Renderer2) {
   }
   // передаем 1 -ый параметр в декортаор строку,
@@ -15,10 +17,12 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    this.r.setStyle(this.el.nativeElement, 'color', this.color)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
   }
 }
