c9f4dac8f478624d60eedc88f9ebaacd004bd9c3
Comprehensher <boltankove@gmail.com>
2023-10-01 17:16:50 +0300

    Add dynamic - when we point mouse on text it becomes blue, when we leave mouse it becomes usual

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -6,15 +6,19 @@ import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
-    // el это доступ до элемента где будет этот аттрибут
-    // доступ будем получать через nativeElement
-    // например меняем стиль элемента
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
   }
   // передаем 1 -ый параметр в декортаор строку,
   // название события, которое мы хотим слушать на элементе куда добавлена дирректива
   // 2-ой параметр, массив обычный js-event через target получаем parent html element
   @HostListener('click', ['$event.target']) onClick(event: Event) {
     console.log(event)
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null)
   }
 }
