06e8b4954866a57cd4cc8ed75346b68ccdbc09fb
Comprehensher <boltankove@gmail.com>
2023-10-01 16:57:13 +0300

    Compnent-directive (which plays role as attribuute of html element) changes style of parent html element - only for WEB appl.

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,15 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef} from "@angular/core";
 
 // use [] when it need to create not tag but attribute
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private el: ElementRef) {
+    // el это доступ до элемента где будет этот аттрибут
+    // доступ будем получать через nativeElement
+    console.log(el);
+    // например меняем стиль элемента
+    el.nativeElement.style.color = 'red'
+  }
 }
