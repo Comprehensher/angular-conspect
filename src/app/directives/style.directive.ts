9448e5f10468838b81e7752fd5a0716684f7d5bf
Comprehensher <boltankove@gmail.com>
2023-10-01 17:12:12 +0300

    Add js event(Mouse click) to compnent-directive for having dynamic

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 // use [] when it need to create not tag but attribute
 @Directive({
@@ -10,5 +10,11 @@ export class StyleDirective {
     // доступ будем получать через nativeElement
     // например меняем стиль элемента
     this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+  // передаем 1 -ый параметр в декортаор строку,
+  // название события, которое мы хотим слушать на элементе куда добавлена дирректива
+  // 2-ой параметр, массив обычный js-event через target получаем parent html element
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event)
   }
 }
