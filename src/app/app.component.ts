46c344346c1e2e2e26d79ccb7cc2529ab9d07365
Comprehensher <boltankove@gmail.com>
2023-10-05 19:00:37 +0300

    Initial commit before Services section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import {Component} from '@angular/core';
-import {Observable} from "rxjs";
-
-export interface Post {
-  title: string
-  text: string
-}
 
 @Component({
   selector: 'app-root',
@@ -12,15 +6,5 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    p: Promise<string> = new Promise<string>(resolve => {
-      setTimeout(() => {
-        resolve('Promise Resolved')
-      }, 4000)
-    })
 
-  date: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(() => {
-      obs.next(new Date())
-    }, 1000)
-  })
 }
