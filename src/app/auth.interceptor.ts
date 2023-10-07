43c23ac852818899dd4a3c3c6bd0b4803c25d406
Comprehensher <boltankove@gmail.com>
2023-10-07 21:33:38 +0300

    Modificate request and response using Interseptors

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,14 +1,29 @@
 // по сути это как Service
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 export class AuthInterceptor implements HttpInterceptor{
   // перехватываем request
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercept request', req)
+
+    // мы не модифицируем существующий реквест, мы его клонируем
+    // а потом передаем клон для дальнейшей обработки
+    // а вот в методе clone мы можем модифицировать клонированный реквест
+    const cloned = req.clone({
+      // добавим новый header
+      headers: req.headers.append('Auth', 'Some random token')
+    })
     // далее мы продолжаем request, но уже с модифицированными данными
     // если не вызовем handle то request не сможет продолжиться
-    return next.handle(req);
+    return next.handle(cloned).pipe(
+      // с помощью tap получаем response, и тоже можем его как-то модифицировать
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Interseptor response', event)
+        }
+      })
+    );
   }
 
 }
