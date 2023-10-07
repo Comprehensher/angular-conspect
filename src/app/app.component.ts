b8808a597737059f57f652f7c626523a8f838f1f
Comprehensher <boltankove@gmail.com>
2023-10-07 11:57:33 +0300

    Initial clean commit before section Work with server(HttpClient)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {MyValidators} from "./my.validators";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  appState = 'on'
-
-  handleChange() {
-    console.log(this.appState)
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
   }
+
 }
