9c7b84bfc97271e9b3b3323f3d94544efd726e3d
Comprehensher <boltankove@gmail.com>
2023-10-07 10:41:38 +0300

    Initial clean commit before lesson your NgModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,6 @@ import {MyValidators} from "./my.validators";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  form: FormGroup
+export class AppComponent {
 
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required,
-        MyValidators.restrictedEmails
-      ], [MyValidators.uniqEmail]), // ассинхронные валидаторы добавдялются в третий параметр
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      address: new FormGroup({
-        country: new FormControl('by'),
-        city: new FormControl('Минск', Validators.required)
-
-      }),
-      skills: new FormArray([])
-    })
-  }
-
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form)
-      const formData = {...this.form.value}
-      console.log('Form Data', formData)
-      // for cleaning all html elements of ther form
-      this.form.reset();
-    }
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Куев',
-      by: 'Минск'
-    }
-
-    const cityKey = this.form.get('address').get('country').value
-    const city = cityMap[cityKey]
-
-    this.form.patchValue({address: {city}})
-  }
-
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    // кастим к типу FormArray - первый способ
-    // (<FormArray>this.form.get('skills')).push()
-    //  Второй способ ниже
-    (this.form.get('skills') as FormArray).push(control)
-  }
 }
