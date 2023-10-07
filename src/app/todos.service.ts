09e74fc8b38da72e2fbe71a173b6bac62bbceecd
Comprehensher <boltankove@gmail.com>
2023-10-07 20:22:51 +0300

    Parameter Observe. We can 1) get response instead of only response body 2) get event of sending receiving query, so as we could debug different elemnets 3) change response type

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {catchError, delay, Observable, throwError} from "rxjs";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
+import {catchError, delay, map, Observable, tap, throwError} from "rxjs";
 
 export interface Todo {
   completed: boolean
@@ -31,12 +31,21 @@ export class TodosService {
     params = params.append('custom', 'anything')
 
     return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
-      params
+      params,
+      // здесь мы можем явно указывать какой тип данных мы хотим получить в ответе. по умолчанию body
+      // если response - то мы получаем целый response, а не тольк его body, и соотственно мы можем получать другие
+      // другие параметры response - такие как status, и т.д. и можем с ними работать
+      observe: 'response'
     })
       // pipe просто для иммитирования долгой загрузки
       // ловим ошибку в pipe если они есть
       // и возвращаем Observable в котором обернута ошибка
       .pipe(
+        // мы не возвращаем тип Observable<Todo[]>, поэтому нужно преобразовать данные с помощью map
+        map(response => {
+          // console.log('Response', response)
+          return response.body
+        }),
         delay(500),
         catchError(error => {
           console.log('Error: ', error.message)
@@ -45,15 +54,34 @@ export class TodosService {
         )
   }
 
-  removeTodo(id: number): Observable<void> {
+  removeTodo(id: number): Observable<any> {
     // В конце указываем id элемента, который необходимо удалить
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      // получаем доступ до всех событий, которые происходят с ассинхронным запросом:
+      // такие как: запрос подгоотовлен, запрос отправлен, запрос пришел и т.д.
+      // на каждое событие мы получаем event, который мы можем каким-то образом обрабатывать
+      observe: 'events'
+    }).pipe(
+      // оператор tap() перехватывает промежуточные данные и мы можем с ними как-то взаимодейстовать
+      tap(event => {
+        if (event.type === HttpEventType.Sent) {
+          console.log('Sent', event)
+        }
+
+        if (event.type === HttpEventType.Response) {
+          console.log('Response', event)
+        }
+      })
+    )
   }
 
-  completeTodo(id: number): Observable<Todo> {
+  completeTodo(id: number): Observable<any> {
     // в body указываем те поля которые хотим модифицировать
     return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       completed: true
+    }, {
+      // json - по умолчанию
+      responseType: 'json'
     })
   }
 }
