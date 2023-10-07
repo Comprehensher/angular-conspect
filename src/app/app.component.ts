188d3e108d0ebabd0334b57fea2fc9e2e7777748
Comprehensher <boltankove@gmail.com>
2023-10-07 12:33:22 +0300

    Get response objects from server. And display them at html page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
+export interface Todo {
+  completed: boolean
+  title: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,7 +13,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
+  constructor(private http: HttpClient) {
+  }
   ngOnInit(): void {
+    // указывам get в <> в каком типе возвращать объекты Response
+
+    // this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    //   .subscribe(response => {
+    //     console.log('Response', response)
+    //     this.todos = response
+    //   })
+
+    // можем перезаписать вместо response todos , еквиалентно закомментированной команде
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        console.log('Response', todos)
+        this.todos = todos
+      })
   }
 
 }
