7b8ebe59897fac01dc047cd503053587fde8cb48
Comprehensher <boltankove@gmail.com>
2023-10-08 14:39:30 +0300

    Create inner router within a component. By clicking mouse, we render child component in definite place of html page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,21 @@ import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {PostsComponent} from "./posts/posts.component";
 import {PostComponent} from "./post/post.component";
+import {AboutExtraComponent} from "./about-extra/about-extra.component";
 
 // http://localhost:4200/ - по такому пути нам необходимо зарегистировать HomeComponent
 // http://localhost:4200/about - по такому пути нам необходимо зарегистировать AboutComponent
 // http://localhost:4200/posts - по такому пути нам необходимо зарегистировать PostsComponent
+// http://localhost:4200/about/extra - по такому пути нам необходимо зарегистировать AboutExtraComponent
 const routes: Routes = [
   // в path указываем путь, который будет зарегистрирован для определенной страницы
   // второй параметр - на этот роут мы хотим чтобы открывался компонент HomeComponent
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  // укажем у страницы About вложенный route
+  {path: 'about', component: AboutComponent, children: [
+      // у children точно также объявляется router
+      {path: 'extra', component: AboutExtraComponent}
+    ]},
   {path: 'posts', component: PostsComponent},
   // говорим, что наше приложение будет еще обрабатывать какую-то динамику
   // после /  будет добавляться какое-то число, которое будет постоянно разным - :id
