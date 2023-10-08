1e07e42bf63fb9a5baa8e7a28859ebdb0491c72f
Comprehensher <boltankove@gmail.com>
2023-10-08 11:22:19 +0300

    Show id of post only on that case when we have quer paramter in url(after ?) that says we need to show id of post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {PostsService} from '../posts.service'
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+  showIds = false
+  constructor(
+    public postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // получаем Router и его queryparams которые является стримом, и нужно на него подписаться
+    this.route.queryParams.subscribe((params: Params) => {
+      // !! - так мы конвертируем строку в bolean значение, сначала инверсия, а потом мы приводим его к соотв резултату
+      this.showIds = !!params['showIds']
+    })
+  }
 }
