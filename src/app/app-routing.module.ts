8ef434985338baddf923cdba76a16951fbac6d85
Comprehensher <boltankove@gmail.com>
2023-10-08 10:02:51 +0300

    Dynamic roouters. It need to dynamicaly build path url to definite component knowing its id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {PostsComponent} from "./posts/posts.component";
+import {PostComponent} from "./post/post.component";
 
 // http://localhost:4200/ - по такому пути нам необходимо зарегистировать HomeComponent
 // http://localhost:4200/about - по такому пути нам необходимо зарегистировать AboutComponent
@@ -12,7 +13,10 @@ const routes: Routes = [
   // второй параметр - на этот роут мы хотим чтобы открывался компонент HomeComponent
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'posts', component: PostsComponent}
+  {path: 'posts', component: PostsComponent},
+  // говорим, что наше приложение будет еще обрабатывать какую-то динамику
+  // после /  будет добавляться какое-то число, которое будет постоянно разным - :id
+  {path: 'posts/:id', component: PostComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
