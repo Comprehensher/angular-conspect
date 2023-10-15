57d372aefc92c18b3e75d0b4c7e1e4572495d2e8
Comprehensher <boltankove@gmail.com>
2023-10-15 09:55:28 +0300

    Initial clean commit before section Dynamic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,19 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AppRoutingModule} from './app-routing.module'
-
-import {SharedModule} from "./shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
+    AppComponent
 
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    SharedModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
-
