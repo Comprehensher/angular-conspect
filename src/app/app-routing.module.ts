b4661f6e771bda190182eeebaf7519e05b478cc2
Comprehensher <boltankove@gmail.com>
2023-10-26 20:41:18 +0300

    Adding main layout and its children. Adding main component and its children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+import {PostPageComponent} from "./post-page/post-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      // если путь пустой, то вначале мы загружаем layout,
+      // но чтобы мы загрузили нужную страницу HomePageComponent, у которой тоже пустой путь указываем redirect
+      // / - будет указывать на HomePageComponent
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
