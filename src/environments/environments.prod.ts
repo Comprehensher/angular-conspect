3c837e4da3b56283a9610ccc7748843ab1dc504a
Comprehensher <boltankove@gmail.com>
2023-10-30 09:31:56 +0200

    Adjust firebase using site https://firebase.google.com - for generation virtual db

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDe2L2_WcgpxOecJrPhDI_Jbah5Mw0xKMg'
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyDe2L2_WcgpxOecJrPhDI_Jbah5Mw0xKMg'
+}
