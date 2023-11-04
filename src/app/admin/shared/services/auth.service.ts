7ca1ee59485381f82f656558fac22fffca1975fb
Comprehensher <boltankove@gmail.com>
2023-11-04 14:42:25 +0200

    Save token in local storage, and specify its expiration

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {FbAuthResponse, User} from "../../../shared/interfaces";
 import {Observable, tap} from "rxjs";
-import {environment} from "../../../../environments/environments";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthService {
@@ -10,25 +10,42 @@ export class AuthService {
   }
 
   get token(): string {
-    return ''
+    const expDate = new Date(localStorage.getItem('fb-token-exp'))
+    // если текущая дата больше чем expDate
+    // и поэтому нам необхожимо его очистить,чтобы кинуть пользователя на logout
+    if (new Date() > expDate) {
+      this.logout()
+      return null
+    }
+    return localStorage.getItem('fb-token', )
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
       )
   }
 
-  logut() {
-
+  logout() {
+    this.setToken(null)
   }
   // данный метод будет нам говорить, авторизован ли пользователь в системе либо нет
   isAuthenticated(): boolean {
     return !!this.token
   }
 
-  private setToken(response: FbAuthResponse) {
-    console.log(response)
+  private setToken(response: FbAuthResponse | null) {
+    // если есть response, т.е не null
+    if (response) {
+      // дополнительный плюс конвертирует к числу
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000)
+      localStorage.setItem('fb-token', response.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+    } else {
+      localStorage.clear()
+    }
+
   }
 }
