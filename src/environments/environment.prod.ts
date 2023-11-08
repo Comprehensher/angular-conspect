db366e76cbdadbb39b8f7ff9685692843d4adf10
Comprehensher <boltankove@gmail.com>
2023-11-08 21:17:42 +0200

    Creation of Post object

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyDe2L2_WcgpxOecJrPhDI_Jbah5Mw0xKMg'
+  apiKey: 'AIzaSyDe2L2_WcgpxOecJrPhDI_Jbah5Mw0xKMg',
+  fbDbUrl: 'https://angular-blog-96793-default-rtdb.firebaseio.com'
 }
