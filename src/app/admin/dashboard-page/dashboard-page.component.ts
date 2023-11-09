7f5b0ef51f3fe25b4df4c6cff7b74995ddccf0ee
Comprehensher <boltankove@gmail.com>
2023-11-09 13:22:04 +0200

    Show list of Posts at the page.

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../shared/services/auth.service";
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  posts: Post[] = []
+  pSub: Subscription
+
+  constructor(private postsService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts =>{
+      this.posts = posts
+    })
+  }
+
+  remove(id: string) {
+
+  }
+
+  // удаляем подписку чтобы избежать утечек памяти
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
 }
